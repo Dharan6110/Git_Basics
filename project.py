e98ea0a70d571ae3ab50678df3ec6dc952dc8246
Dharan6110 <dharaneeswarreddybrahmadevunni@gmail.com>
2023-12-02 11:24:59 +0530

    Added Project.py

--- project.py
+++ project.py
@@ -1,6 +1,17 @@
-a = int(input("Enter a Value :______"))
-b = int(input("Enter a Value :______"))
+# a = int(input("Enter a Value :______"))
+# b = int(input("Enter a Value :______"))
 
-c = a**b
+# c = a**b
 
-print(f"The value of {a}**{b} is", c)
+# print(f"The value of {a}**{b} is", c)
+
+def fibonacci(n):
+    fib_series = [0, 1]
+    while len(fib_series) < n:
+        fib_series.append(fib_series[-1] + fib_series[-2])
+    return fib_series
+
+# Example: Generate the first 10 Fibonacci numbers
+n = 10
+result = fibonacci(n)
+print(f"Fibonacci series of first {n} numbers: {result}")
